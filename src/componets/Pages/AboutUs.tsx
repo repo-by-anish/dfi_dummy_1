984b2a7160e941b198bc9a7bb07be7c30a7772ac
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2024-02-10 03:51:12 +0530

    Updated

---
+++ src/componets/Pages/AboutUs.tsx
@@ -0,0 +1,38 @@
+const AboutUs = (): JSX.Element => {
+
+    return (
+        <div className="about_us">
+            <h1>About Us</h1>
+            <p className="abt_text">At Diamond Fruit International (DFI), we take pride in offering a diverse and exotic selection of fresh, organic fruits sourced from around the globe. Our commitment to quality and freshness ensures that each fruit is a flavorful delight, bringing a touch of international goodness to your doorstep.</p>
+            <div className="abt_type_name">
+                <h2>Explore Our Range</h2>
+                <div className="abt_item">
+                    <h3>Tropical Treasures</h3>
+                    <p>Mangoes: Sourced from the sun-kissed orchards, our mangoes are a burst of tropical sweetness.
+                        Pineapples: Indulge in the juicy and refreshing taste of our premium pineapples.</p>
+                </div>
+                <div className="abt_item">
+                    <h3>Citrus Symphony</h3>
+                    <p>Oranges: Enjoy the zesty and vitamin-packed goodness of our handpicked oranges.
+                        Lemons and Limes: Elevate your culinary creations with the tangy brilliance of our lemons and limes.</p>
+                </div>
+                <div className="abt_item">
+                    <h3>Berries Galore</h3>
+                    <p>Strawberries: Delight in the succulent sweetness of our plump and juicy strawberries.
+                        Blueberries: Packed with antioxidants, our blueberries add a burst of color and health to your diet.</p>
+                </div>
+                <div className="abt_item">
+                    <h3>Exotic Delights</h3>
+                    <p>Dragon Fruit: Experience the visually stunning and subtly sweet taste of dragon fruit.
+                        Passion Fruit: Unleash the exotic flavor of passion fruit in your beverages and desserts.</p>
+                </div>
+                <div className="abt_item">
+                    <h3>Stone Fruit Elegance:</h3>
+                    <p>Peaches and Plums: From orchards known for their stone fruits, our peaches and plums are juicy and delectable.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs
